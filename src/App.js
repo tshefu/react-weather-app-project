dc24ff6ea34e487f4746c950eb4a888e8752a913
tshefu <schneider.viktoria95@gmail.com>
2022-05-28 13:24:55 +0200

    added the GitHub URL for the repository

--- src/App.js
+++ src/App.js
@@ -1,5 +1,18 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App </h1>
+      <p>
+        This project was coded by Viktoria Schneider and is{" "}
+        <a
+          href="https://github.com/tshefu/react-weather-app-project"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </p>
+    </div>
+  );
 }
