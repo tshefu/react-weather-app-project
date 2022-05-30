9cb915b0169caaaa893bc944fb23b763ac492352
tshefu <schneider.viktoria95@gmail.com>
2022-05-30 12:57:04 +0200

    added basic styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
 import "./App.css";
+import Weather from "./Weather";
 
 export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <h1> Weather App </h1>
-        <p>
-          This project was coded by Viktoria Schneider and is{" "}
-          <a
-            href="https://github.com/tshefu/react-weather-app-project"
-            target="_blank"
-          >
-            open-sourced on GitHub
-          </a>
-        </p>
+        <Weather />
+        <footer>
+          <p>
+            This project was coded by Viktoria Schneider and is{" "}
+            <a
+              href="https://github.com/tshefu/react-weather-app-project"
+              target="_blank"
+            >
+              open-sourced on GitHub
+            </a>
+          </p>
+        </footer>
       </div>
     </div>
   );
