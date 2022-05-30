d167d5eb48fbc9973a988c350a874e91b5800a42
tshefu <schneider.viktoria95@gmail.com>
2022-05-30 10:59:57 +0200

    added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,16 +3,18 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather App </h1>
-      <p>
-        This project was coded by Viktoria Schneider and is{" "}
-        <a
-          href="https://github.com/tshefu/react-weather-app-project"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </p>
+      <div className="container">
+        <h1> Weather App </h1>
+        <p>
+          This project was coded by Viktoria Schneider and is{" "}
+          <a
+            href="https://github.com/tshefu/react-weather-app-project"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </p>
+      </div>
     </div>
   );
 }
