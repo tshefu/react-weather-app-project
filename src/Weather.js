fa5bfa5ccc2e82db79199214e6fc9e1067521689
tshefu <schneider.viktoria95@gmail.com>
2022-06-03 00:38:57 +0200

    added matching icons

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humdity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+      icon: response.data.weather[0].icon,
     });
   }
 
