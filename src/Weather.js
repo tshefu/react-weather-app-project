6c0777f0ecb17f0a056904cf6bb4a19387bc5f01
tshefu <schneider.viktoria95@gmail.com>
2022-06-03 00:03:55 +0200

    used the correct icon URL

--- src/Weather.js
+++ src/Weather.js
@@ -18,7 +18,7 @@ export default function Weather(props) {
       wind: response.data.wind.speed,
       humdity: response.data.main.humidity,
       description: response.data.weather[0].description,
-      iconUrl: "https://ssl.gstatic.com/onebox/weather/64/sunny.png",
+      iconUrl: `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
     });
   }
 
