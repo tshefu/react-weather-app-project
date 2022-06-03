74fe4a587564042d90746a34751977c6219c4d3e
tshefu <schneider.viktoria95@gmail.com>
2022-06-03 22:07:07 +0200

    changed the default city to Vienna

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default function App() {
   return (
     <div className="App">
       <div className="container">
-        <Weather defaultCity="New York" />
+        <Weather defaultCity="Vienna" />
         <footer>
           <p>
             This project was coded by
